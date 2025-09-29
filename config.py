1de2d083fec74fa8dee03e20696d37a34f4a0351
OttoKto <118813620+OttoKto@users.noreply.github.com>
2025-09-30 01:02:15 +0200

    Update config.py
    del keys

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-bybit_key = 'tdVnGwlbI5O1vIUTGT'
-bybit_secret = '6GWl2vR0c8iXKyYfNKKZXo8TkKmPeeQ8m0EU'
-twitter_key = 'new1_b254c56b257e4d128dabbbb6f0adafdc'
+bybit_key = ''
+bybit_secret = ''
+twitter_key = ''
